2783fd7eca1cca5471fe4266933b7642c4904fc3
Jacek Banaszczyk <jacek.banaszczyk@gmail.com>
2018-12-28 18:03:48 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# CCFinderX
+
+Repository contains yet another clone of [CCFinderx](http://www.ccfinder.net)
+
+## AIST CCFinderX
+
+AIST CCFinderX is a code-clone detector, which detects code clones (duplicated code fragments) from source files written in Java, C/C++, COBOL, VB, C#.
---
+++ doc/Latest/the archive of CCFinder Official Site_pliki/tabmaker.js
@@ -0,0 +1,58 @@
+﻿function generateTabs(tabIndex, tabTitles, tabUrls, topTitle) {
+	document.write("<div align=\"center\"><font size=\"+3\">" + topTitle + "</font></div>");
+	document.write("<br />");
+
+	document.write("<center>");
+	document.write("<table cellspacing=\"0\" width=\"100%\">");
+	
+	tabs = 5;
+	document.write("<tr>");
+	document.write("<td height=\"2\"></td>");
+	for (i = 0; i < tabTitles.length; ++i) {
+		document.write("<td style=\"background:#303030\"></td>");
+		document.write("<td style=\"background:#303030\"></td>");
+		document.write("<td style=\"background:#303030\"></td>");
+		if (i + 1 < tabTitles.length) {
+			document.write("<td height=\"2\"></td>");
+		}
+	}
+	document.write("<td></td>");
+	document.write("</tr>");
+	
+	document.write("<tr>");
+	document.write("<td width=\"7%\"></td>");
+	for (i = 0; i < tabTitles.length; ++i) {
+		document.write("<td style=\"background:#303030\" width=\"2\"></td>");
+		document.write("<td style=\"text-align:center\">");
+		document.write("<a href=\"" + tabUrls[i] + "\" style=\"text-decoration:none;\">");
+		document.write(tabTitles[i])
+		document.write("</a>");
+		document.write("</td>");
+		document.write("<td style=\"background:#303030\" width=\"2\"></td>");
+		if (i + 1 < tabTitles.length) {
+			document.write("<td width=\"2%\"></td>");
+		}
+	}
+	document.write("<td width=\"7%\"></td>");
+	document.write("</tr>");
+	
+	document.write("<tr>");
+	document.write("<td style=\"background:#303030\" height=\"2\"></td>");
+	for (i = 0; i < tabTitles.length; ++i) {
+		document.write("<td style=\"background:#303030\"></td>");
+		if (i == tabIndex) {
+			document.write("<td></td>");
+		} else {
+			document.write("<td style=\"background:#303030\"></td>");
+		}
+		document.write("<td style=\"background:#303030\"></td>");
+		if (i + 1 < tabTitles.length) {
+			document.write("<td style=\"background:#303030\"></td>");
+		}
+	}
+	document.write("<td style=\"background:#303030\"></td>");
+	document.write("</tr>");
+	
+	document.write("</table>");
+	document.write("</center>");
+}
